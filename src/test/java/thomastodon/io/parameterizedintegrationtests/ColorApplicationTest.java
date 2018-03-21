0a2abcbc439c0f3b09309d0af401d844b171b15a
thomasshouler <thomas.shouler@gmail.com>
2018-03-20 20:55:50 -0500

    Upgrade to boot 2 and junit 5

--- src/test/java/thomastodon/io/parameterizedintegrationtests/ColorApplicationTest.java
+++ src/test/java/thomastodon/io/parameterizedintegrationtests/ColorApplicationTest.java
@@ -1,33 +1,23 @@
 package thomastodon.io.parameterizedintegrationtests;
 
-import junitparams.JUnitParamsRunner;
-import junitparams.Parameters;
-import org.junit.ClassRule;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.rules.SpringClassRule;
-import org.springframework.test.context.junit4.rules.SpringMethodRule;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@RunWith(JUnitParamsRunner.class)
-public class ColorApplicationTest {
-
-    @Rule public final SpringMethodRule springMethodRule = new SpringMethodRule();
-    @ClassRule public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
+@ExtendWith(SpringExtension.class)
+class ColorApplicationTest {
 
     @Autowired ColorService colorService;
 
-    @Test
-    @Parameters({
-        "red, yellow",
-        "blue, green",
-    })
-    public void contextLoads(String colorOne, String colorTwo) {
+    @ParameterizedTest
+    @CsvSource({"red, yellow", "blue, green"})
+    void contextLoads(String colorOne, String colorTwo) {
         assertThat(colorService.add(colorOne, colorTwo)).isEqualTo("purple");
     }
 }
